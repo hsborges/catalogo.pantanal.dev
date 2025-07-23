b01f52e9b408b0ebb22d9c0860842e2154b0202d
Hudson Silva Borges <hudsonsilbor@gmail.com>
2025-07-22 21:57:48 -0300

    feat: add FilmeSeeder to populate database with initial Filme entries

---
+++ src/main/java/dev/pantanal/catalogo/seeders/FilmeSeeder.java
@@ -0,0 +1,57 @@
+package dev.pantanal.catalogo.seeders;
+
+import dev.pantanal.catalogo.filmes.Filme;
+import dev.pantanal.catalogo.filmes.FilmeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+@Component
+public class FilmeSeeder implements CommandLineRunner {
+    private final FilmeRepository filmeRepository;
+
+    public FilmeSeeder(FilmeRepository filmeRepository) {
+        this.filmeRepository = filmeRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (filmeRepository.count() == 0) {
+            Filme superman = Filme.builder()
+                    .titulo("Superman")
+                    .diretor("James Gunn")
+                    .lancamento(LocalDate.of(2025, 7, 10))
+                    .genero("Ação, Aventura, Ficção Científica")
+                    .classificacao(14)
+                    .duracaoMinutos(129)
+                    .elenco(Arrays.asList(
+                            "David Corenswet", "Rachel Brosnahan", "Nicholas Hoult", "Nathan Fillion",
+                            "Isabela Merced"))
+                    .distribuidora("Warner Bros.")
+                    .capaUrl(
+                            "https://m.media-amazon.com/images/M/MV5BZTk2YjUxODgtMTgxYS00NmQyLTk0OTctNmNhYjZjNTc0NDg1XkEyXkFqcGc@._V1_QL75_UY562_CR1,0,380,562_.jpg")
+                    .trailerUrl("https://www.imdb.com/pt/video/vi311543833/")
+                    .build();
+
+            Filme quarteto = Filme.builder()
+                    .titulo("Quarteto Fantástico: Primeiros Passos")
+                    .diretor("Matt Shakman")
+                    .lancamento(LocalDate.of(2025, 7, 24))
+                    .genero("Ação, Aventura, Ficção Científica")
+                    .classificacao(12)
+                    .duracaoMinutos(115)
+                    .elenco(Arrays.asList(
+                            "Pedro Pascal", "Vanessa Kirby", "Ebon Moss-Bachrach", "Joseph Quinn", "Ralph Ineson"))
+                    .distribuidora("Marvel Studios")
+                    .capaUrl(
+                            "https://m.media-amazon.com/images/M/MV5BYmY3NGMxNjgtMTM1My00OGZhLTg0NzMtNDY1YzFiNTcxNDA5XkEyXkFqcGc@._V1_QL75_UX380_CR0,0,380,562_.jpg")
+                    .trailerUrl("https://www.imdb.com/pt/video/vi3218196505/")
+                    .build();
+
+            filmeRepository.save(superman);
+            filmeRepository.save(quarteto);
+        }
+    }
+}
